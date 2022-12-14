9a694122f85194ab9e3c1d13616a8af231cb6ef3
Tobey Blaber <contact@tobey.me>
2022-12-14 04:20:10 +0000

    Initial commit

---
+++ Tobey.BepInExTweaks.Subnautica/BepInExTweaks.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace Tobey.BepInExTweaks.Subnautica;
+
+[BepInDependency("com.bepis.bepinex.configurationmanager", BepInDependency.DependencyFlags.SoftDependency)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class BepInExTweaks : BaseUnityPlugin
+{
+    public static BepInExTweaks Instance { get; private set; }
+
+    private void Awake()
+    {
+        // enforce singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+}
---
+++ Tobey.BepInExTweaks.Subnautica/Config.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx.Configuration;
+
+namespace Tobey.BepInExTweaks.Subnautica;
+public static class Config
+{
+    internal static ConfigFile Cfg => BepInExTweaks.Instance.Config;
+
+    public static class General
+    {
+        //public static ConfigEntry<KeyboardShortcut> ShowConfigManager { get; } = 
+    }
+}
